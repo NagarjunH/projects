3a3b04d6fd3572cc1c46d810599a30faa7f6aa62
Nagarjun Honrao <nagarjun.honrao.5@gmail.com>
2026-05-08 14:05:12 +0530

    Comments Added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
 
+// Create express application
 const app = express();
 
 app.get("/",(req,res)=>{
     res.send("Hello World !")
 });
 
+
+// Server is running on port 5000
 app.listen(5000, ()=>{
     console.log("Server is running on port 5000")
 })
