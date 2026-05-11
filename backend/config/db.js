324735a9e43cfd6aa2028447cb7e82ae842758dd
Nagarjun Honrao <nagarjun.honrao.5@gmail.com>
2026-05-11 23:24:05 +0530

    Done The proper connection with DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,5 +6,18 @@ dotenv.config();
 
 const pool = new Pool({
     host: process.env.DB_HOST,
-    
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 })
+
+pool.on("connect",()=>{
+    console.log("Connected to the databse");
+})
+
+pool.on("error",(err)=>{
+    console.error("Database Error", err)
+});
+
+export default pool;
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
 
 // Create express application
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
 
 app.get("/",(req,res)=>{
     res.send("Hello World !")
 });
 
+const PORT = process.env.PORT || 5000;
 
 // Server is running on port 5000
-app.listen(5000, ()=>{
-    console.log("Server is running on port 5000")
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
 })
